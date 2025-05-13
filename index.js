f7f1b6fdb5bc08399611bfa6e47ca0dd1e727553
Ramasamy3488 <kvpramasamy@gmail.com>
2025-05-13 16:00:43 +0530

    vercel_v3

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+// api/index.js
 const express = require("express");
 const app = express();
+const serverless = require("serverless-http");
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => {
+  res.send("<h1>Hello from Express deployed on Vercel!</h1>");
+});
 
+// Add more routes here if needed
 
 module.exports = app;
+module.exports.handler = serverless(app);
