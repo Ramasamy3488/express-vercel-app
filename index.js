9ccef6ec3e515ca97ee2ce6c2906a46a6a392493
Ramasamy3488 <kvpramasamy@gmail.com>
2025-05-13 16:55:11 +0530

    vercel_v7

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config(); // Load environment variables from .env
-
+const serverless = require('serverless-http');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -102,6 +102,7 @@ app.delete('/api/users/deleteuser', async (req, res) => {
   }
 });
 
+
 // Handle unmatched routes
 app.use((req, res) => {
   res.status(404).json({ error: 'Route not found' });
@@ -109,8 +110,8 @@ app.use((req, res) => {
 
 // Start the server
 
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+// app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
 
 // Optional for testing
-// module.exports = app;
+module.exports = app;
 
